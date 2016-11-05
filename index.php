4f6d691ef8159e52606d206f4a5d8d6ac66aa486
bitscoder1498 <abhi.guha.jss97@gmail.com>
2016-11-05 14:05:22 +0530

    Add files via upload

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-
+<!doctype html>
+<html>
+<head>
+<meta charset ="utf -8">
+<title> JQuery Example</title>
+</head>
+<body>
+<p onclick="$(this) . hide() ;"> Hello World!</p>
+<script type="text/javascript" src="js/jquery-3.1.1.min.js"></script>
+</body>
+</html>
