4819fef490460569661fc75ca19613ae92f02556
bitscoder1498 <abhi.guha.jss97@gmail.com>
2016-11-05 22:55:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+app.get('/ui/231fe88.jpg', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', '231fe88.jpg'));
+});
+
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
